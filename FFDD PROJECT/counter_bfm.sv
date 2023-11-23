1ce5312a4d093567e600d941c952af233e64001f
L SATHYA <128246519+suryal30103@users.noreply.github.com>
2023-11-23 12:18:42 +0530

    Add files via upload

---
+++ FFDD PROJECT/counter_bfm.sv
@@ -0,0 +1,38 @@
+class counter_bfm;
+
+	virtual counter_intf intf;
+	mailbox gen2bfm;
+	int no_transactions;
+
+	function new(virtual counter_intf intf, mailbox gen2bfm);
+		this.intf = intf;
+		thus.gen2bfm = gen2bfm;
+	endfunction
+
+	
+	task reset;
+		wait(intf.rst);
+		$display("Rsest Initiated");
+		intf.bfm_cb.load <= 0;
+		intf.bfm_cb.updown <= 0;
+		intf.bfm_cb.data <= 0;
+		intf.bfm_cb.data_out <= 0;
+		wait(!intf.rst);
+		$display("Reset Finished");
+	endtask;
+
+	task main;
+		forever begin
+		counter_trans trans;
+		gen2bfm.get(trans);
+		$display("Transaction No. = %0d , no_transactions);
+		intf.bfm_cb.load <= trans.load;
+		intf.bfm_cb.updown <= trans.updown;
+		intf.bfm_cb_data <= trans.data;
+		repeat(2) @(posedge intf.clk);
+		trans.data_out = intf.bfm_cb.data_out;
+		trans.display();
+		no_transactions++;
+	end
+	endtask
+endclass
---
+++ FFDD PROJECT/tb_counter_top.sv
@@ -0,0 +1,17 @@
+module tb_counter_top;
+    bit clk;
+    bit rst;
+
+    counter_intf intf(clk,rst);
+    counter_test test(intf);
+
+    counter dut(.clk(intf.clk),.rst(intf.rst),.load(intf.load),.updown(rst.updown),.data(intf.data),.data_out(intf.data_out));
+
+    alwasy #5 clk=~clk;
+
+    initial begin
+        rst=1;
+        #5;
+        rst=0;
+    end
+endmodule
